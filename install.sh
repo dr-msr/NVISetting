f6f840c5d432753658e72fb3ac8cf370a1968ead
drmsr <me@drmsr.dev>
2024-04-07 03:49:41 +0800

    update psutil

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
  sudo apt update && sudo apt upgrade -y
- sudo apt install python3 python3-gi python3-pip
+ sudo apt install python3 python3-gi python3-pip -y
+ python3 -m pip install psutil
  cd Panels && sh ./configure && sudo make install && xfce4-panel-profiles load Default.tar.bz2 
  git config --global user.email "me@drmsr.dev"
  git config --global user.name "drmsr"
