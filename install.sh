35a6979a74526ff01e28823dbe81be4a20e3d609
drmsr <me@drmsr.dev>
2024-04-07 03:38:01 +0800

    init

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
  sudo apt update && sudo apt upgrade -y
  sudo apt install python3 python3-gi python3-pip
  cd Panels && sh ./configure && sudo make install && xfce4-panel-profiles load Default.tar.bz2 
+ git config --global user.email "me@drmsr.dev"
+ git config --global user.name "drmsr"
