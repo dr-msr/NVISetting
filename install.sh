5429aafa766758fee631f7b204e0790ae72d3bd1
drmsr <me@drmsr.dev>
2024-04-07 06:15:58 +0800

    cleanup

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@ echo '
   Pin: origin packages.mozilla.org
   Pin-Priority: 1000
   ' | sudo tee /etc/apt/preferences.d/mozilla
-sudo apt-get install wget gpg
+sudo apt install wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
-
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install python3 python3-gi pip code telegram-desktop firefox-devedition nodejs -y
